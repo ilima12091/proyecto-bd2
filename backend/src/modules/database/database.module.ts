131f07fed6bdf5d50fbe56a6738e6562c90bca19
Esteves, Tomás NBK2995 <tomas.esteves@gsoft.com.uy>
2024-06-20 23:30:00 -0300

    Datos para pool conexion base desde config

--- backend/src/modules/database/database.module.ts
+++ backend/src/modules/database/database.module.ts
@@ -8,23 +8,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   providers: [
     {
       provide: 'PG_CONNECTION',
-      /*useFactory: async (configService: ConfigService) => {
+      useFactory: async (configService: ConfigService) => {
         const pool = new Pool({
           user: configService.get<string>('DATABASE_USER'),
           host: configService.get<string>('DATABASE_HOST'),
           database: configService.get<string>('DATABASE_NAME'),
           password: configService.get<string>('DATABASE_PASSWORD'),
           port: configService.get<number>('DATABASE_PORT'),
-        });
-        return pool;
-      },*/
-      useFactory: async (configService: ConfigService) => {
-        const pool = new Pool({
-          user: 'postgres',
-          host: '127.0.0.1',
-          database: 'pencaucu',
-          password: 'admin',
-          port: 5432,
         });
         return pool;
       },
