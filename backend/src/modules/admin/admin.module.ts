bc62c47a4a59021d35abd35324f6797fb61fdd6d
Esteves, Tomás NBK2995 <tomas.esteves@gsoft.com.uy>
2024-06-20 23:26:57 -0300

    Matriz CRUD para tabla pais

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -3,10 +3,19 @@ import { AdminController } from './controllers/admin/admin.controller';
 import { AdminService } from './services/admin/admin.service';
 import { DatabaseModule } from '../database/database.module';
 import { AdminModel } from './models/admin.model';
+import { CountryController } from './controllers/country/country.controller';
+import { CountryService } from './services/country/country.service';
+import { CountryModel } from './models/country.model';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AdminController],
-  providers: [AdminService, AdminModel],
+  controllers: [
+    AdminController, 
+    CountryController
+  ],
+  providers: [
+    AdminService, AdminModel,
+    CountryService, CountryModel
+  ],
 })
 export class AdminModule {}
---
+++ backend/src/modules/admin/types/country.type.ts
@@ -0,0 +1,5 @@
+export type Country = {
+  id: number;
+  nombre: string;
+  confederacion: string;
+};
