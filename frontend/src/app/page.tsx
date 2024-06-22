ef8cc3c4983e664b6616d8240ed29c82a82b46d6
ignacio.lima <ignacio.lima@inswitch.com>
2024-06-22 18:27:12 -0300

    Se agrega una lógica de sesión

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,31 +5,34 @@ import PredictionItem from "./components/PredictionItem/PredictionItem";
 import ErrorAlert from "@/components/ErrorAlert/ErrorAlert";
 import useGetPredictions from "@/hooks/useGetPredictions";
 import "./styles.css";
+import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 
 export default function Home() {
   const { error, data, isLoading, isLoaded, getPredictions } = useGetPredictions(1);
 
   return (
-    <main className="predictions">
-      {isLoading && <p>Cargando partidos...</p>}
-      {error && <ErrorAlert errorText="Error cargando partidos" />}
-      {isLoaded && !error && (
-        <>
-          <h1 className="predictions-title">Partidos</h1>
-          {data?.length > 0 ? (
-            <List
-              data={data}
-              ItemComponent={PredictionItem}
-              className="predictions-list"
-              itemProps={{
-                refetchData: getPredictions,
-              }}
-            />
-          ) : (
-            <p>No hay partidos para mostrar</p>
-          )}
-        </>
-      )}
-    </main>
+    <ProtectedRoute>
+      <main className="predictions">
+        {isLoading && <p>Cargando partidos...</p>}
+        {error && <ErrorAlert errorText="Error cargando partidos" />}
+        {isLoaded && !error && (
+          <>
+            <h1 className="predictions-title">Partidos</h1>
+            {data?.length > 0 ? (
+              <List
+                data={data}
+                ItemComponent={PredictionItem}
+                className="predictions-list"
+                itemProps={{
+                  refetchData: getPredictions,
+                }}
+              />
+            ) : (
+              <p>No hay partidos para mostrar</p>
+            )}
+          </>
+        )}
+      </main>
+    </ProtectedRoute>
   );
 }
