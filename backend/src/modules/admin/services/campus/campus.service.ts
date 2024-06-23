2bc1e08a76b1e47d31eaafdeb599bda430db0152
Esteves, Tomás NBK2995 <tomas.esteves@gsoft.com.uy>
2024-06-23 20:14:18 -0300

    Validar id url y body para PUT

--- backend/src/modules/admin/services/campus/campus.service.ts
+++ backend/src/modules/admin/services/campus/campus.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CampusModel } from '../../models/campus.model';
 import { CreateCampusDto } from '../../dtos/create-campus.dto';
+import { CountryService } from '../country/country.service';
 
 @Injectable()
 export class CampusService {
-  constructor(private campusModel: CampusModel) {}
+  constructor(private campusModel: CampusModel,
+              private countryService: CountryService
+  ) {}
 
   async getAll() {
     return await this.campusModel.getAll();
@@ -27,6 +30,10 @@ export class CampusService {
   }
 
   async update(id: number, createCampusDto: CreateCampusDto) {
+    if (id !== createCampusDto.id) {
+      throw new Error('El id del body y la url no coinciden');
+    }
+
     const { city, state, countryId } = createCampusDto;
 
     const updateData = await this.campusModel.update(
