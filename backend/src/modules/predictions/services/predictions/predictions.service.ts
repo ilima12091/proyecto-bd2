82efe8fe59fa3f6692d1ff3e0c5089b2e3188f6e
ignacio.lima <ignacio.lima@inswitch.com>
2024-06-26 23:13:33 -0300

    Se arregla un error del rebase

--- backend/src/modules/predictions/services/predictions/predictions.service.ts
+++ backend/src/modules/predictions/services/predictions/predictions.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PredictionModel } from '../../models/prediction.model';
-<<<<<<< HEAD
 import { CreatePredictionDto } from '../../dtos/create-prediction.dto';
-=======
->>>>>>> 2ce0e06 (Se agrega parte de la pantalla de predicciones y el módulo de predicciones sin terminar en el backend)
 
 @Injectable()
 export class PredictionsService {
@@ -12,7 +9,6 @@ export class PredictionsService {
   async getPredictionsByUserId(userId: number) {
     return await this.predictionModel.getPredictionsByUserId(userId);
   }
-<<<<<<< HEAD
 
   async createOrUpdatePrediction(
     userId: number,
@@ -44,22 +40,18 @@ export class PredictionsService {
   async createChampionAndRunnerUpPrediction(
     userId: number,
     championId: number,
-    runnerUpId: number
+    runnerUpId: number,
   ) {
     return await this.predictionModel.createChampionAndRunnerUpPrediction(
       userId,
       championId,
-      runnerUpId
+      runnerUpId,
     );
   }
 
   async loadPredictionPoints(
     userId: number,
     userLocalGoals: number,
-    userAwayGoals: number
-  ) {
-
-  }
-=======
->>>>>>> 2ce0e06 (Se agrega parte de la pantalla de predicciones y el módulo de predicciones sin terminar en el backend)
+    userAwayGoals: number,
+  ) {}
 }
