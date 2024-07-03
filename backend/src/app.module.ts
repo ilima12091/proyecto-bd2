7f7716c601d23e97d45fe58b2b1bdf6a2310c083
Facundo Martínez <facusml16@gmail.com>
2024-07-03 16:28:05 -0300

    Resuelto rebase con master

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AdminModule } from './modules/admin/admin.module';
 import { DatabaseModule } from './modules/database/database.module';
-import { StudentsModule } from './modules/students/students.module';
+
 import { PredictionsModule } from './modules/predictions/predictions.module';
+<<<<<<< HEAD
 import { MatchesModule } from './modules/matches/matches.module';
+=======
+import { StudentsModule } from './modules/students/students.module';
+
+>>>>>>> 717cf5d (Resuelto conflicto con origin 15 registro)
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -12,9 +17,12 @@ import { MatchesModule } from './modules/matches/matches.module';
     }),
     AdminModule,
     DatabaseModule,
-    StudentsModule,
     PredictionsModule,
+<<<<<<< HEAD
     MatchesModule,
+=======
+    StudentsModule
+>>>>>>> 717cf5d (Resuelto conflicto con origin 15 registro)
   ],
   controllers: [],
   providers: [],
